5fb1d4073a0bcabb0b065263fd53ce4778bb360c
Julia Kramskoy <julia.kramskoy@gmail.com>
2019-11-04 19:37:54 -0600

    added changes to html file

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from flask  import Flask,render_template
+import datetime
 
 app = Flask(__name__)
-
+year = datetime.datetime.now().year
+name = "Julia"
+cities = ["Edmonton","Winnipeg","Toronto"]
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html",year=year,name=name,cities=cities)
+
 
 @app.route('/about-me')
 def about():
-    return render_template("about.html")
+    return render_template("about.html",cities=cities)
 
 
 if __name__ == '__main__':
