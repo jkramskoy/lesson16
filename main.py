b5846a69cdfbf329298b445b891cca787e57cfc0
Julia Kramskoy <julia.kramskoy@gmail.com>
2019-11-04 19:05:58 -0600

    added about me html file

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/about-me')
+def about():
+    return render_template("about.html")
+
+
 if __name__ == '__main__':
    app.run(debug=True,port=5006)
 
